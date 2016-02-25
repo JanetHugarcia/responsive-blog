fc6e86d16e79982138e5ea32c176f4d7619da385
JanetHugarcia <nayavz@gmail.com>
2016-02-25 12:07:39 -0500

    version 2

--- jquery/mostrar-nav.js
+++ jquery/mostrar-nav.js
@@ -1,3 +1,13 @@
 $('#mostrar-nav').on('click', function(){
 	$('nav').toggleClass('mostrar');
-})
+});
+
+var altura = $('header').offset().top;
+
+$(window).on('scroll', function(){
+	if ( $(window).scrollTop() > altura ){
+		$('header').addClass('menu-fixed');
+	} else {
+		$('header').removeClass('menu-fixed');
+	}
+});
